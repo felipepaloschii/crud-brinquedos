746149b8b73364810ec766674bf67092793415a9
felipe_paloschi <felipe_paloschi@estudante.sesisenai.org.br>
2026-09-23 09:00:02 -0300

    add - delete

---
+++ public/delete.php
@@ -0,0 +1,19 @@
+<?php
+
+include "../infra/conexao.php";
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM brinquedos WHERE id = ?";
+
+$stmt = $conexao->prepare($sql);
+
+$stmt->bind_param("i", $id);
+
+$stmt->execute();
+
+header("Location: ../index.php");
+
+exit;
+
+?>
